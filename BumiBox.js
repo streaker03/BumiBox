aca837ce1d4c7f283c2e6c06082ccf018744ff1e
streaker03 <andrewgostomelsky@gmail.com>
2020-04-26 02:03:12 -0400

    version 1

--- BumiBox.js
+++ BumiBox.js
@@ -9,11 +9,13 @@ const ytsr = require("ytsr");
 let announcement = "general";
 let videos = [];
 let names = [];
-let connection = bot;
+let connectionOut = bot;
 let paused = false;
 let playing = false;
+let dispatcherOut = bot;
 
 function changeChannel(channelName){
+    console.log(channelName);
     announcement = channelName;
 }
 
@@ -21,59 +23,66 @@ let options = {
     limit: 1
 };
 
-async function playLink(word, msgChannel) {
+async function playLink(word, msgChannel, author) {
     let data = ytdl.getBasicInfo(word);
     await data.then(function(value) {
         names.push(value.player_response.videoDetails.title + " by " + value.player_response.videoDetails.author);
         videos.push(ytdl(word, {quality: "highestaudio", highWatermark: 1 << 30}));
         msgChannel.send("\`Added " + value.player_response.videoDetails.title + " by " + value.player_response.videoDetails.author + "\`");
     });
-    if(!paused) {
-        broadcast();
-    } else {
-        connection.resume();
+    if(!paused && !playing) {
+        await broadcast(author);
+    } else if(paused) {
+        dispatcherOut.resume();
         paused = false;
     }
 
 }
-async function playSearch(word, msgChannel) {
+async function playSearch(word, msgChannel, author) {
     let data = ytsr(word, options);
     await data.then(function(value) {
         names.push(value.items[0].title.toString() + " by " + value.items[0].author.name.toString());
         videos.push(ytdl(value.items[0].link.toString(), {quality: "highestaudio", highWaterMark: 1 << 30}));
         msgChannel.send("\`Added " + value.items[0].title.toString() + " by " + value.items[0].author.name.toString() + "\`");
     });
-    if(!paused) {
-        broadcast();
-    } else {
-        connection.resume();
+    if(!paused && !playing) {
+        await broadcast(author);
+    } else if(paused) {
+        dispatcherOut.resume();
         paused = false;
     }
 }
 
-function broadcast() {
-    if(videos.length > 0 && !playing) {
-        connection.play(videos[0]);
+async function broadcast(author) {
+    await author.voice.channel.join().then(connection => {
+        const dispatcher = connection.play(videos[0]);
+        dispatcherOut = dispatcher;
+        connectionOut = connection;
         playing = true;
-    }
-}
-
-async function join(author) {
-    connection = await author.voice.channel.join();
+        dispatcher.on("finish", () => {
+            videos.shift();
+            names.shift();
+            if(videos.length > 0) {
+                broadcast(author);
+            } else {
+                connection.disconnect();
+            }
+        })
+    });
 }
 
 function pause() {
-    connection.then(sound => {
-        connection.pause();
-    });
+    dispatcherOut.pause();
     paused = true;
     playing = false;
+    return "\`Paused\`";
 }
 
 function resume() {
-    connection.resume();
+    dispatcherOut.resume();
     paused = false;
     playing = true;
+    return "\`Resuming...\`";
 }
 
 function queue() {
@@ -96,48 +105,50 @@ bot.on("message", msg => {
     let prefix = settings.prefix;
     if(msg.author.bot) {return;}
     if(msg.channel.name !== announcement) {return;}
+    if(msg.member.voice.channel == null) {return;}
     if(msg.content.startsWith(prefix + "channel ")) {
-        let word = msg.content.substring(msg.content.indexOf(" "), msg.content.length);
-        changeChannel(word)
+        let word = msg.content.substring(msg.content.indexOf(" ")+1, msg.content.length);
+        changeChannel(word);
     }
     if(msg.content.startsWith(prefix + "play ")) {
-        let word = msg.content.substring(msg.content.indexOf(" "), msg.content.length);
+        let word = msg.content.substring(msg.content.indexOf(" ")+1, msg.content.length);
         if(msg.embeds.length > 0) {
-            playLink(word, msg.channel);
+            playLink(word, msg.channel, msg.member);
         } else {
-            playSearch(word, msg.channel);
+            playSearch(word, msg.channel, msg.member);
         }
     }
-    if(msg.content.startsWith(prefix + "join")) {
-        join(msg.member);
-    }
     if(msg.content.startsWith(prefix + "pause")) {
-        pause();
+        msg.channel.send(pause());
     }
     if(msg.content.startsWith(prefix + "queue")) {
         msg.channel.send(queue());
     }
     if(msg.content.startsWith(prefix + "remove ")) {
-        let index = msg.content.substring(msg.content.indexOf(" "), msg.content.length);
+        let index = msg.content.substring(msg.content.indexOf(" ")+1, msg.content.length);
         msg.channel.send(remove(parseInt(index)));
     }
     if(msg.content.startsWith(prefix + "stop")) {
         videos = [];
         names = [];
-        connection.disconnect();
+        playing = false;
+        paused = false;
+        connectionOut.disconnect();
+        msg.channel.send("\`Stopped\`");
     }
     if(msg.content.startsWith(prefix + "skip")) {
         msg.channel.send("\`Skipped " + names[0] + "\`");
         videos.shift();
         names.shift();
         if(videos.length > 0) {
-            connection.play(videos[0]);
+            broadcast(msg.member);
         } else {
+            connectionOut.disconnect();
             playing = false;
         }
     }
     if(msg.content.startsWith(prefix + "resume")) {
-        resume();
+        msg.channel.send(resume());
     }
 });
 
